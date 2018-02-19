1b7c4e7aca45113433c8f5186f6690f0b8482014
Matt <mathu99@gmail.com>
2018-02-19 08:02:00 +0200

    feat: how it works page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,29 +7,29 @@ import { Component } from '@angular/core';
 export class HomeComponent {
     title = 'app';
   properties = {
-    tyreWidths: [
-      '12', '13', '14', '15', '16', '17', '18', '19'
+    wheelSizes: [
+      '16', '17', '18', '19'
     ],
     tyreProfiles: [
-      '30', '40', '50', '60', '70'
+      '30', '40', '55'
     ],
-    tyreSizes: [
-      '250', '260', '270', '280', '290', '300'
+    tyreWidths: [
+      '195', '245', '255'
     ],
     quantities: [
       '1', '2', '3', '4'
     ],
     brands: [
-      'Any', 'Dunlop', 'Goodyear', 'Michelin', 'Pirelli', 'Firestone', 'Hankook', 'Toyo'
+      'Any', 'Bridgestone', 'Continental', 'Dunlop', 'Goodyear', 'Pirelli', 'Yokohama'
     ],
     locations: [
-      'Any', 'Cape Town', 'Durban', 'Johannesburg', 'Pretoria'
+      'Any', 'Gauteng'
     ],
   };
   data = {
     width: this.properties.tyreWidths[0],
     profile: this.properties.tyreProfiles[0],
-    size: this.properties.tyreSizes[0],
+    size: this.properties.wheelSizes[0],
     quantity: this.properties.quantities[0],
     brand: this.properties.brands[0],
     location: this.properties.locations[0],
