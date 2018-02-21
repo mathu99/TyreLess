51de33f4986106689257c34137661de97dbb6d86
Matt <mathu99@gmail.com>
2018-02-21 14:21:02 +0200

    feat: spinner for results

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Http } from '@angular/http';
+import { get, set, assign } from 'lodash';
 
 @Component({
   selector: 'app-search',
@@ -12,11 +13,8 @@ export class SearchComponent {
     private router: Router, private http: Http) { this.http = http }
   collapsed = true;
   sub = null;
-  properties = {
-    searching: false,
-    valid: false,
-  }
-  data = null;
+  properties = <any>{};
+  data = <any>{};
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
@@ -28,15 +26,23 @@ export class SearchComponent {
         this.data = JSON.parse(JSON.stringify(params)); /* Copy object */
         this.data.wheelAlignmentChecked = this.data.wheelAlignmentChecked == "true";
         this.data.wheelBalancingChecked = this.data.wheelBalancingChecked == "true";
-        if (this.properties['vehicleTypes'] != undefined) {
+        if (get(this.properties, 'vehicleTypes') != undefined) {
           this.data.selectedSrc = this.properties['vehicleTypes'].filter(e => e.name === this.data['selected'])[0].imageSrc;
         }
+        this.search();
       });
     this.http.get('environments/config.development.json').subscribe(res => {
-      this.properties = res.json().properties;
+      assign(this.properties,  res.json().properties)
       this.data.selectedSrc = this.properties['vehicleTypes'].filter(e => e.name === this.data['selected'])[0].imageSrc;
     });
   }
+
+  search = () => {
+    set(this.properties, 'loading', true);
+    setTimeout(() => {
+      set(this.properties, 'loading', false);
+    }, 2000);
+  };
 
   update = (property, value) => {
     this.data[property] = value;
