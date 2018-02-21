fefde2b96007abd70e75654fab0d6faae2b44c35
Matt <mathu99@gmail.com>
2018-02-21 11:33:29 +0200

    feat: wip search results

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,37 +6,8 @@ import { Http } from '@angular/http';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  properties = {
-    wheelSizes: [
-      '16', '17', '18', '19'
-    ],
-    tyreProfiles: [
-      '30', '40', '55'
-    ],
-    tyreWidths: [
-      '195', '245', '255'
-    ],
-    quantities: [
-      '1', '2', '3', '4'
-    ],
-    brands: [
-      'Any', 'Bridgestone', 'Continental', 'Dunlop', 'Goodyear', 'Pirelli', 'Yokohama'
-    ],
-    locations: [
-      'Any', 'Gauteng'
-    ],
-  };
-  data = {
-    width: this.properties.tyreWidths[0],
-    profile: this.properties.tyreProfiles[0],
-    size: this.properties.wheelSizes[0],
-    quantity: this.properties.quantities[0],
-    brand: this.properties.brands[0],
-    location: this.properties.locations[0],
-    selected: 'car',
-    wheelAlignmentChecked: false,
-    wheelBalancingChecked: false,
-  };
+  properties = null;
+  data = null;
   collapsed = true;
 
   toggleCollapsed(): void {
@@ -46,6 +17,17 @@ export class HomeComponent {
   constructor(private http: Http) {
     this.http.get('environments/config.development.json').subscribe(res => {
       this.properties = res.json().properties;
+      this.data = {
+        width: this.properties.tyreWidths[0],
+        profile: this.properties.tyreProfiles[0],
+        size: this.properties.wheelSizes[0],
+        quantity: this.properties.quantities[0],
+        brand: this.properties.brands[0],
+        location: this.properties.locations[0],
+        selected: this.properties.vehicleTypes[0].name,
+        wheelAlignmentChecked: false,
+        wheelBalancingChecked: false,
+      };
     });
   }
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-search',
@@ -8,14 +9,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SearchComponent {
   constructor(
     private route: ActivatedRoute,
-    private router: Router) {}
-    collapsed = true;
-    sub = null;
-    properties = {
-      searching: false,
-      valid: false,
-    }
-    data = null;
+    private router: Router, private http: Http) { this.http = http }
+  collapsed = true;
+  sub = null;
+  properties = {
+    searching: false,
+    valid: false,
+  }
+  data = null;
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
@@ -24,9 +25,22 @@ export class SearchComponent {
     this.sub = this.route
       .queryParams
       .subscribe(params => {
-        this.data = params;
+        this.data = JSON.parse(JSON.stringify(params)); /* Copy object */
+        this.data.wheelAlignmentChecked = this.data.wheelAlignmentChecked == "true";
+        this.data.wheelBalancingChecked = this.data.wheelBalancingChecked == "true";
+        if (this.properties['vehicleTypes'] != undefined) {
+          this.data.selectedSrc = this.properties['vehicleTypes'].filter(e => e.name === this.data['selected'])[0].imageSrc;
+        }
       });
+    this.http.get('environments/config.development.json').subscribe(res => {
+      this.properties = res.json().properties;
+      this.data.selectedSrc = this.properties['vehicleTypes'].filter(e => e.name === this.data['selected'])[0].imageSrc;
+    });
   }
+
+  update = (property, value) => {
+    this.data[property] = value;
+  };
 
   ngOnDestroy() {
     this.sub.unsubscribe();
